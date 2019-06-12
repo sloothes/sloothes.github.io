f65ee196d5e089db712f250ee05b90c984abd054
Jane Slooth <sloothes.com@gmail.com>
2019-06-12 08:13:02 +0300

    Update menu-list.js

--- menu-list.js
+++ menu-list.js
@@ -2,6 +2,23 @@
 
     var list = {
         "Menu": {
+            "Sex": [
+                ["Male", "/gender/male"],
+                ["Female", "/gender/female"],
+            ],
+            "Actions": [
+                ["Idle", "/action/idle"],
+                ["Walk", "/action/walk"],
+                ["Run",  "/action/run"],
+                ["Jump", "/action/jump"],
+            ],
+            "Controls": [
+                ["Back",  "/turn/back"],
+                ["Left",  "/turn/left"],
+                ["Right", "/turn/right"],
+                ["Front", "/turn/front"],
+            ],
+
             "User": [
                 ["Login", "/user/login"],
                 ["Signup", "/user/signup"],
